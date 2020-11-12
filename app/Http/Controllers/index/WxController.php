e01d8cbd9f7f3685b39cfdadd7d72515957bd98a
dagedaya <2>
2020-11-12 20:18:51 +0800

    1

--- app/Http/Controllers/index/WxController.php
+++ app/Http/Controllers/index/WxController.php
@@ -490,7 +490,7 @@ class WxController extends Controller
         $api="https://api.weixin.qq.com/sns/userinfo?access_token=".$access_token."&openid=".$openid."&lang=zh_CN";
         $user=file_get_contents($api);
         $user_info=json_decode($user,true);
-        dd($user_info);
+//        dd($user_info);
     }
 
 
