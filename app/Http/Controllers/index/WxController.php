ed7ea03c1f3cb4c4be8df49ca47179bad244f115
dagedaya <2>
2020-11-12 11:13:16 +0800

    修改

--- app/Http/Controllers/index/WxController.php
+++ app/Http/Controllers/index/WxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\index;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use App\Model\WxUserModel;
--- app/Http/Controllers/index/WxTestController.php
+++ app/Http/Controllers/index/WxTestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\index;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Log;
